f12f4c0201df385003eb447e27899632ff244a60
jucheolkang <jucheo9877@naver.com>
2023-11-06 18:37:24 +0900

    build : 프로젝트 초기 설정

---
+++ src/main/java/com/project/schedulesharing/ScheduleSharingApplication.java
@@ -0,0 +1,13 @@
+package com.project.schedulesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleSharingApplication.class, args);
+	}
+
+}
